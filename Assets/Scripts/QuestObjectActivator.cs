9095e5fe67885b25667c911de3daa1ae779e00c6
Robert Murillo <110421148+RobertMurillo89@users.noreply.github.com>
2024-05-14 14:13:21 -0700

    Activating objects for completed quests

---
+++ Assets/Scripts/QuestObjectActivator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObjectActivator : MonoBehaviour
+{
+
+    public GameObject objectToActivate;
+
+    public string questToCheck;
+
+    public bool activeIfComplete;
+
+    bool initialCheckDone;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!initialCheckDone)
+        {
+            initialCheckDone = true;
+
+            CheckCompletion();
+        }
+    }
+
+    public void CheckCompletion()
+    {
+        if (QuestManager.instance.CheckIfComplete(questToCheck))
+        {
+            objectToActivate.SetActive(activeIfComplete);
+
+        }
+    }
+
+    
+}
