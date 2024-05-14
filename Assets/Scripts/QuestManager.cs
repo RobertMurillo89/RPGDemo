b5a8c570837a9ad1228853d57a7ca8384eba108a
Robert Murillo <110421148+RobertMurillo89@users.noreply.github.com>
2024-05-14 13:02:26 -0700

    Making, Checking, Marking Quests comp

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public string[] questMarkerNames;
+    public bool[] questMarkersComplete;
+
+    public static QuestManager instance;
+
+    void Start()
+    {
+        instance = this;
+
+        questMarkersComplete = new bool[questMarkerNames.Length];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log(CheckIfComplete("Quest Test"));
+            MarkQuestComplete("Quest Test");
+            MarkQuestIncomplete("Fight the Demon");
+        }
+    }
+
+    public int GetQuestNumber(string questToFind)
+    {
+        for(int i = 0;i<questMarkerNames.Length; i++)
+        {
+            if (questMarkerNames[i] == questToFind)
+            {
+                return i;
+            }
+        }
+
+        Debug.LogError("Quest " + questToFind + " does not exist");
+        return 0;
+    }
+
+    public bool CheckIfComplete(string questToCheck)
+    {
+        if(GetQuestNumber(questToCheck) != 0)
+        {
+            return questMarkersComplete[GetQuestNumber(questToCheck)];
+        }
+
+        return false;
+    }
+
+    public void MarkQuestComplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = true;
+
+
+    }
+
+    public void MarkQuestIncomplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = false;
+
+    }
+}
