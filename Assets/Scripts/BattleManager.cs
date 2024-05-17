0c7874a2f40203a9ee10cb321606b9549bac4fcb
Robert Murillo <110421148+RobertMurillo89@users.noreply.github.com>
2024-05-17 14:02:21 -0700

    load in players and enemies into battlescene

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -16,7 +16,8 @@ public class BattleManager : MonoBehaviour
     public BattleChar[] playerPrefabs;
     public BattleChar[] enemyPrefabs;
 
-
+    public List<BattleChar> activeBattlers = new List<BattleChar>();
+    
     void Start()
     {
         instance = this;
@@ -28,7 +29,7 @@ public class BattleManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.T))
         {
-            BattleStart(new string[] { "EyeBall", "Spider" });
+            BattleStart(new string[] { "EyeBall", "Spider", "Skeleton" });
         }
     }
 
@@ -45,6 +46,48 @@ public class BattleManager : MonoBehaviour
             battleScene.SetActive(true);
 
             AudioManager.instance.PlayBGM(0);
+
+            for(int i = 0; i < playerPositions.Length; i++)
+            {
+                if (GameManager.instance.playerStats[i].gameObject.activeInHierarchy) 
+                {
+                    for(int j = 0; j < playerPrefabs.Length; j++)
+                    {
+                        if (playerPrefabs[j].charName == GameManager.instance.playerStats[i].charName)
+                        {
+                            BattleChar newPlayer = Instantiate(playerPrefabs[j], playerPositions[i].position, playerPositions[i].rotation);
+                            newPlayer.transform.parent = playerPositions[i];
+
+                            activeBattlers.Add(newPlayer);
+                            CharStats thePlayer = GameManager.instance.playerStats[i];
+                            activeBattlers[i].currentHP = thePlayer.currentHp;
+                            activeBattlers[i].maxHP = thePlayer.maxHP;
+                            activeBattlers[i].currentMP = thePlayer.currentMP;
+                            activeBattlers[i].maxMP = thePlayer.maxMP;
+                            activeBattlers[i].strength = thePlayer.strength;
+                            activeBattlers[i].defence = thePlayer.defence;
+                            activeBattlers[i].wpnPower = thePlayer.wpnPwr;
+                            activeBattlers[i].armrPower = thePlayer.armrPwr;
+                        }
+                    } 
+                }
+            }
+
+            for (int i = 0; i < enemiesToSpawn.Length; i++)
+            {
+                if (enemiesToSpawn[i] != "")
+                {
+                    for(int j = 0; j < enemyPrefabs.Length; j++)
+                    {
+                        if (enemyPrefabs[j].charName == enemiesToSpawn[i])
+                        {
+                            BattleChar newEnemy = Instantiate(enemyPrefabs[j], enemyPositions[i].position, enemyPositions[i].rotation);
+                            newEnemy.transform.parent = enemyPositions[i];
+                            activeBattlers.Add(newEnemy);
+                        }
+                    }
+                }
+            }
         }
     }
 }
